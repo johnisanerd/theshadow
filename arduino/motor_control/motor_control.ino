52e5ab6d35b321a62e4de84a8c11c2b3a2cef30e
John Is A Nerd <admin@dexterindustries.com>
2017-09-24 11:55:10 +0400

    Adding Motor Controls - All Pins
    And documentation of Arduino MEGA

--- arduino/motor_control/motor_control.ino
+++ arduino/motor_control/motor_control.ino
@@ -1,14 +1,14 @@
 int relay2 = 2;   // 1st relay
 int relay3 = 3;   // 2nd relay
 
-int m1_a = 52;
-int m1_b = 50;
-int m2_a = 48;
-int m2_b = 46;
-int m3_a = 44;
-int m3_b = 42;
-int m4_a = 40;
-int m4_b = 38;
+int m1_a = 52;  // M1 Forward   // Purple
+int m1_b = 50;  // M1 Backward  // Grey wire
+int m2_a = 48;  // M2 Forward   // Green 
+int m2_b = 46;  // M2 Backward  // Blue
+int m3_a = 44;  // M3 Forward   // Orange
+int m3_b = 42;  // M3 Backward  // Yellow
+int m4_a = 40;  // M4 Forward   // Brown
+int m4_b = 38;  // M4 Backward  // Red
 
 void setup()
 {
@@ -36,41 +36,50 @@ void setup()
   
 }
 
-void m1_fwd(){
-  // digitalWrite(relay2, HIGH);   // sets the LED on
-  // digitalWrite(relay3, LOW);
-
-  digitalWrite(m1_a, HIGH);   // sets the LED on
-  digitalWrite(m1_b, LOW);
-  
+void motor_fwd(int a, int b){
+  digitalWrite(a, HIGH);   // sets the LED on
+  digitalWrite(b, LOW);
 }
 
-void m1_bwd(){
-  // digitalWrite(relay2, LOW);    
-  // digitalWrite(relay3, HIGH);
-  digitalWrite(m1_a, LOW);    
-  digitalWrite(m1_b, HIGH);
-  
+void motor_bwd(int a, int b){
+  digitalWrite(a, LOW);   // sets the LED on
+  digitalWrite(b, HIGH);
 }
 
-void m1_stp(){
-  // digitalWrite(relay2, HIGH);    
-  // digitalWrite(relay3, HIGH);
-
-  digitalWrite(m1_a, HIGH);    
-  digitalWrite(m1_b, HIGH);
-  
+void motor_stop(int a, int b){\
+  digitalWrite(a, HIGH);    
+  digitalWrite(b, HIGH);
   delay(2000);
+}
+
+void all_motors_stop(){
+  digitalWrite(m1_a, HIGH);
+  digitalWrite(m1_b, HIGH);
+  digitalWrite(m2_a, HIGH);    
+  digitalWrite(m2_b, HIGH);
+  digitalWrite(m3_a, HIGH);    
+  digitalWrite(m3_b, HIGH);
+  digitalWrite(m4_a, HIGH);    
+  digitalWrite(m4_b, HIGH);
 }
 
 void loop()
 {
-  m1_fwd();
+  motor_fwd(m1_a, m1_b);
+  motor_fwd(m2_a, m2_b);
+  motor_fwd(m3_a, m3_b);
+  motor_fwd(m4_a, m4_b);
   delay(5000);                  // waits for a second
-  m1_stp();
-
-  m1_bwd();
+  motor_stop(m1_a, m1_b);
+  motor_stop(m2_a, m2_b);
+  motor_stop(m3_a, m3_b);
+  motor_stop(m4_a, m4_b);
+    
+  motor_bwd(m1_a, m1_b);
+  motor_bwd(m2_a, m2_b);
+  motor_bwd(m3_a, m3_b);
+  motor_bwd(m4_a, m4_b);  
   delay(5000);                  // waits for a second
-  m1_stp();
-
+  all_motors_stop();
+  delay(1000);
 }
