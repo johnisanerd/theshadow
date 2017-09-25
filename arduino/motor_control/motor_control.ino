679e83c3f5240a4a30fd36be6258865337005d5a
John Is A Nerd <admin@dexterindustries.com>
2017-09-25 15:39:11 +0400

    Adding relay controls

--- arduino/motor_control/motor_control.ino
+++ arduino/motor_control/motor_control.ino
@@ -1,5 +1,3 @@
-int relay2 = 2;   // 1st relay
-int relay3 = 3;   // 2nd relay
 
 int m1_a = 52;  // M1 Forward   // Purple
 int m1_b = 50;  // M1 Backward  // Grey wire
@@ -10,11 +8,21 @@ int m3_b = 42;  // M3 Backward  // Yellow
 int m4_a = 40;  // M4 Forward   // Brown
 int m4_b = 38;  // M4 Backward  // Red
 
+int m4_forw_limit = 23;
+int m4_back_limit = 27;
+int m4_forw_mag   = 25;
+int m4_back_mag   = 29;
+
+int status_m4_forw_limit = false;
+int status_m4_back_limit = false;
+int status_m4_forw_mag   = false;
+int status_m4_back_mag   = false;
+
+
+bool debug = true;
+
 void setup()
 {
-  pinMode(relay2, OUTPUT);      // sets the digital pin as output
-  pinMode(relay3, OUTPUT);
-
   pinMode(m1_a, OUTPUT);
   pinMode(m1_b, OUTPUT);
   pinMode(m2_a, OUTPUT);
@@ -33,7 +41,34 @@ void setup()
   digitalWrite(m3_b, HIGH);
   digitalWrite(m4_a, HIGH);
   digitalWrite(m4_b, HIGH);
+
+  pinMode(m4_forw_limit, INPUT);
+  pinMode(m4_back_limit, INPUT);
+  pinMode(m4_forw_mag, INPUT);
+  pinMode(m4_back_mag, INPUT);
+
+  // Open serial communications and wait for port to open:
+  Serial.begin(2000000);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.println("Setup and initialized!");
   
+}
+
+void check_switches(){
+  status_m4_forw_limit = digitalRead(m4_forw_limit);
+  status_m4_back_limit = digitalRead(m4_back_limit);
+  status_m4_forw_mag   = digitalRead(m4_forw_mag);
+  status_m4_back_mag   = digitalRead(m4_back_mag);
+
+  if(debug){
+    Serial.println(" - - - ");
+    /* Serial.print("status_m4_forw_limit: "); */  Serial.println(status_m4_forw_limit, DEC);
+    /* Serial.print("status_m4_back_limit: "); */ Serial.println(status_m4_back_limit, DEC);
+    /* Serial.print("status_m4_forw_mag: "); */ Serial.println(status_m4_forw_mag, DEC);
+    /* Serial.print("status_m4_back_mag: "); */ Serial.println(status_m4_back_mag, DEC);   
+  }
 }
 
 void motor_fwd(int a, int b){
@@ -49,7 +84,7 @@ void motor_bwd(int a, int b){
 void motor_stop(int a, int b){\
   digitalWrite(a, HIGH);    
   digitalWrite(b, HIGH);
-  delay(2000);
+  // delay(2000);
 }
 
 void all_motors_stop(){
@@ -61,25 +96,20 @@ void all_motors_stop(){
   digitalWrite(m3_b, HIGH);
   digitalWrite(m4_a, HIGH);    
   digitalWrite(m4_b, HIGH);
+  delay(1000);
 }
 
 void loop()
 {
-  motor_fwd(m1_a, m1_b);
-  motor_fwd(m2_a, m2_b);
-  motor_fwd(m3_a, m3_b);
-  motor_fwd(m4_a, m4_b);
-  delay(5000);                  // waits for a second
-  motor_stop(m1_a, m1_b);
-  motor_stop(m2_a, m2_b);
-  motor_stop(m3_a, m3_b);
-  motor_stop(m4_a, m4_b);
-    
-  motor_bwd(m1_a, m1_b);
-  motor_bwd(m2_a, m2_b);
-  motor_bwd(m3_a, m3_b);
-  motor_bwd(m4_a, m4_b);  
-  delay(5000);                  // waits for a second
-  all_motors_stop();
-  delay(1000);
+  check_switches();
+
+  if(status_m4_forw_limit){
+    motor_fwd(m4_a, m4_b);
+  }
+  else{
+    motor_stop(m4_a, m4_b);
+  }
+  // delay(5000);                  // waits for a second
+  // all_motors_stop();
+
 }
